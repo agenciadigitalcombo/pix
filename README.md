01b81f8945777eed5c3f2d405023a67da6a039a5
brunocriacoes <br.rafael@outlook.com>
2021-11-12 09:12:06 -0300

    ajuste base

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pix Combo Pay
+
+### Regitar webhook Zoop 
+
+*obs* é necessario cadastrar um webhook para cada *Seler ID*
+
+https://pix.combopay.com.br/api/webhook.php
--- api/webhook.php
+++ api/webhook.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Content-Type: text/html; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+date_default_timezone_set('America/Sao_Paulo');
+
 $request = file_get_contents('php://input');
 $request = json_decode($request, true) ?? [];
 
@@ -9,3 +13,8 @@ $content .= json_encode( array_merge($request, $_REQUEST) );
 $content .= " \n";
 
 file_put_contents($file_path, $content, FILE_APPEND);
+
+echo json_encode([
+    "next" => true,
+    "message" => "Web Hook recebido com sucesso"
+]);
